53c6ad52baa8a849aed704518033a73212bbbd81
Thomas Luppi <thomasluppi@gmail.com>
2020-05-31 17:49:51 -0700

    Some spawn definition cleanup

--- Contracts/Definitions/Events/SpawnDefinition.cs
+++ Contracts/Definitions/Events/SpawnDefinition.cs
@@ -4,22 +4,36 @@
 
 namespace LegendsGenerator.Contracts.Definitions.Events
 {
-    using System;
     using System.Collections.Generic;
-    using System.Text;
 
     using LegendsGenerator.Contracts.Compiler;
 
+    /// <summary>
+    /// A definition of a result which spawns a new thing.
+    /// </summary>
     [UsesAdditionalParametersForHoldingClass]
     public partial class SpawnDefinition : BaseDefinition
     {
+        /// <summary>
+        /// Gets or sets the name of the definition to spawn.
+        /// </summary>
+        public string DefinitionName { get; set; } = "UNSET";
+
+        /// <summary>
+        /// Gets or sets the type of thing to spawn.
+        /// </summary>
         public ThingType Type { get; set; }
 
-        public string DefinitionName { get; set; }
-
+        /// <summary>
+        /// Gets the attribute overrides for this spawn.
+        /// </summary>
+        [CompiledDictionary(typeof(int), "Subject")]
         public Dictionary<string, string> AttributeOverrides { get; } = new Dictionary<string, string>();
 
+        /// <summary>
+        /// Gets or sets the description of this spawn result.
+        /// </summary>
         [Compiled(typeof(string), "Subject", AsFormattedText = true)]
-        public string Description { get; set; }
+        public string Description { get; set; } = "UNSET_DESCRIPTION";
     }
 }
